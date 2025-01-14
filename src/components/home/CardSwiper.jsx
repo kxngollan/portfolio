a88a48993973e88aa7ee40491a4ee9454633e8d5
Ollan Muza <ammmuza@gmail.com>
2025-01-14 21:27:19 +0000

    fix cardswiper, change portfolio projects setup

--- src/components/home/CardSwiper.jsx
+++ src/components/home/CardSwiper.jsx
@@ -1,57 +1,109 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./CardSwiper.css";
 import "./CardImages.css";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const CardSwiper = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const groupsRef = useRef([]);
 
-  const cardGroups = [0, 1, 2, 3];
+  useEffect(() => {
+    groupsRef.current = Array.from(
+      document.getElementsByClassName("card-group")
+    );
+  }, []);
 
-  const handleNextClick = () => {
-    const nextIndex = (activeIndex + 1) % cardGroups.length;
-    setActiveIndex(nextIndex);
+  const handleLoveClick = () => {
+    if (groupsRef.current.length === 0) return;
+
+    const nextIndex =
+      activeIndex + 1 <= groupsRef.current.length - 1 ? activeIndex + 1 : 0;
+
+    const currentGroup = document.querySelector(
+      `[data-index="${activeIndex}"]`
+    );
+    const nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    currentGroup.dataset.status = "after";
+    nextGroup.dataset.status = "becoming-active-from-before";
+
+    setTimeout(() => {
+      nextGroup.dataset.status = "active";
+      setActiveIndex(nextIndex);
+    });
   };
 
-  const handlePrevClick = () => {
-    const prevIndex = (activeIndex - 1 + cardGroups.length) % cardGroups.length;
-    setActiveIndex(prevIndex);
+  const handleHateClick = () => {
+    if (groupsRef.current.length === 0) return;
+
+    const nextIndex =
+      activeIndex - 1 >= 0 ? activeIndex - 1 : groupsRef.current.length - 1;
+
+    const currentGroup = document.querySelector(
+      `[data-index="${activeIndex}"]`
+    );
+    const nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    currentGroup.dataset.status = "before";
+    nextGroup.dataset.status = "becoming-active-from-after";
+
+    setTimeout(() => {
+      nextGroup.dataset.status = "active";
+      setActiveIndex(nextIndex);
+    });
   };
 
   return (
     <div className="card-swiper">
       <div className="card-groups">
-        {cardGroups.map((_, index) => (
-          <div
-            key={index}
-            className={`card-group ${index === activeIndex ? "active" : ""}`}
-          >
-            {Array(8)
-              .fill(0)
-              .map((_, i) => (
-                <div
-                  key={i}
-                  className={`${i % 2 === 0 ? "little-card" : "big-card"} card`}
-                ></div>
-              ))}
-          </div>
-        ))}
+        <div className="card-group" data-index="0" data-status="active">
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+        </div>
+        <div className="card-group" data-index="1" data-status="unknown">
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+        </div>
+        <div className="card-group" data-index="2" data-status="unknown">
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+        </div>
+        <div className="card-group" data-index="3" data-status="unknown">
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+          <div className="little-card card"></div>
+          <div className="big-card card"></div>
+        </div>
       </div>
       <div className="card-swiper-buttons">
-        <button
-          id="prev-button"
-          onClick={handlePrevClick}
-          aria-label="Previous cards"
-        >
+        <button id="hate-button" onClick={handleHateClick}>
           <FaArrowLeft />
         </button>
-        <button
-          id="next-button"
-          onClick={handleNextClick}
-          aria-label="Next cards"
-        >
+        <button id="love-button" onClick={handleLoveClick}>
           <FaArrowRight />
         </button>
       </div>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,34 +1,203 @@
 "use client";
-
-import { useEffect, useState } from "react";
-import ProjectPhotos from "./ProjectPhotos";
+import Link from "next/link";
+import Image from "next/image";
+import { FaGithub } from "react-icons/fa6";
+import { IoLogoFigma } from "react-icons/io5";
+import { FaGlobeAfrica } from "react-icons/fa";
 import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import "./Projects.css";
+import comingSoon from "./assets/mm-coming-soon.png";
 
 const Projects = () => {
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const wrapperRef = useRef(null);
+  const [isDragged, setIsDragged] = useState(false);
 
-  const fetchProjects = async () => {
-    setLoading(true);
-    setError(null);
-    try {
-      console.log("fetching projects");
-      const res = await fetch(`${server}/get/projects`);
-      if (!res.ok) throw new Error("Failed to fetch projects");
+  const vue = [
+    {
+      name: "Chess",
+      description:
+        "A chess game allow users to play and fetch games using Chess.com API",
+      link: "https://chess-weld-rho.vercel.app/",
+      github: "https://github.com/kxngollan/chess",
+    },
+    {
+      name: "Forum",
+      description:
+        "This is message forum board app for users to login and drop a messgae in",
+      link: "https://nuxtapp-sooty.vercel.app/",
+      github: "https://github.com/kxngollan/nuxtapp",
+    },
+  ];
 
-      const data = await res.json();
-      setProjects(data);
-    } catch (err) {
-      console.log(err);
-      setError("Failed to get projects");
-    }
-    setLoading(false);
-  };
+  const react = [
+    {
+      name: "Portfolio",
+      description:
+        "This is my personal portfolio that I created to showcase all of my abilities as a full stack developer. Using a plethora of different technologies to create. I want to be able to have a place where people can come and see all of my work without being overwhelmed nor confused.",
+      link: "https://wwww.ollanmuza.com",
+      github: "https://github.com/kxngollan/portfolio",
+      figma:
+        "https://www.figma.com/file/TSiGHDAfHnW1EZGlD1lR5W/portfolio?type=design&node-id=0-1&mode=design&t=r5DczCSMr1MmdJSy-0",
+    },
+    {
+      name: "FStore",
+      description: "",
+      link: "https://fake-store-red.vercel.app/",
+      github: "https://github.com/kxngollan/fake-store",
+    },
+    {
+      name: "Catch'em ",
+      description:
+        "This is a Pokemon game that I created as a memory game from The Odin project. Why Pokemon? Pokemon has been one of my favourite games to play since I was 8 years so I wanted to create some which would be fun and inquisitive. I use 'pokeapi.co' to source the images",
+      link: "https://chic-gecko-a211b1.netlify.app/",
+      github: "https://github.com/kxngollan/Catch-em-all",
+      figma: "https://github.com/kxngollan/Catch-em-all",
+    },
+    {
+      name: "CV",
+      description:
+        "This is a Cv maker I have created using Vite ReactJs. I used Vite because the load time is considerable faster than regular react. This uses the useState functionality in react to keep store of the inputs and",
+      link: "https://teal-biscuit-78824f.netlify.app/",
+      github: "https://github.com/kxngollan/cv-maker",
+      figma:
+        "https://www.figma.com/file/CBAXUlYq2xCAT6k2EKXnXw/portfolio?type=design&mode=design&t=oTV3yepu6BMZqjrk-1",
+    },
+  ];
+
+  const other = [
+    {
+      name: "color",
+      description:
+        "This is a kids memory game of remember the sequence. The whole point of this game is to the previous colors and see if the player can reenact the complete sequence with each level only having one color added for additional difficult.",
+      link: "https://kxngollan.github.io/Color-Along/",
+      github: "https://github.com/kxngollan/color-along",
+      figma:
+        "https://www.figma.com/file/vMFHsmHFFNfzEfshDtwNhv/Color-Along?type=design&node-id=0%3A1&mode=design&t=xbZGHmaiV5oHn0dr-1",
+    },
+    {
+      name: "Sketch",
+      description:
+        "This is an Etch A Sketch board project that I built as a kid's fun game. I found this project idea on the Odin project and I loved it so much because it reminded me of being a kid travelling and being given one of these to doodle on. It was a lot of fun to make, especially because of all the elaborate creations I can doodle.",
+      link: "https://kxngollan.github.io/etch-a-sketch-page/",
+      github: "https://github.com/kxngollan/etch-a-sketch-page",
+      figma:
+        "https://www.figma.com/file/OBpI2xulWl3y4ktwVVNpFc/Etch-a-sketch?type=design&node-id=0%3A1&mode=design&t=oTV3yepu6BMZqjrk-1",
+    },
+    {
+      name: "KeyDrum",
+      description:
+        "This is a drum app which allows for users to make cool and interesting beats. It is very simple and fun to play with. I I made this to showcase my abilities with vanilla Javascript, html and css.",
+      link: "https://kxngollan.github.io/My-Drum/",
+      github: "https://github.com/kxngollan/My-Drum",
+    },
+  ];
+
+  const display = [
+    {
+      title: "Vue/Nuxt",
+      id: "vue",
+      content: vue,
+    },
+    { title: "React/Next", id: "react", content: react },
+    {
+      title: "Other",
+      id: "other",
+      content: other,
+    },
+  ];
 
   useEffect(() => {
-    fetchProjects();
+    const anchors = document.querySelectorAll('a[href^="#"]');
+    anchors.forEach((anchor) => {
+      anchor.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const targetId = anchor.getAttribute("href");
+        const target = document.querySelector(targetId);
+
+        if (target) {
+          target.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }
+      });
+    });
+
+    return () => {
+      anchors.forEach((anchor) => {
+        anchor.removeEventListener("click", () => {});
+      });
+    };
   }, []);
+
+  useEffect(() => {
+    const slider = wrapperRef.current;
+
+    if (!slider) return;
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    const preventClick = (e) => {
+      e.preventDefault();
+      e.stopImmediatePropagation();
+    };
+
+    const mouseDownHandler = (e) => {
+      isDown = true;
+      slider.classList.add("active");
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    };
+
+    const mouseLeaveHandler = () => {
+      isDown = false;
+      slider.classList.remove("active");
+    };
+
+    const mouseUpHandler = (e) => {
+      isDown = false;
+      if (isDragged) {
+        const projects = slider.querySelectorAll(".project");
+        projects.forEach((project) =>
+          project.addEventListener("click", preventClick)
+        );
+      } else {
+        const projects = slider.querySelectorAll(".project");
+        projects.forEach((project) =>
+          project.removeEventListener("click", preventClick)
+        );
+      }
+      slider.classList.remove("active");
+      setIsDragged(false);
+    };
+
+    const mouseMoveHandler = (e) => {
+      if (!isDown) return;
+      setIsDragged(true);
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 2; // Adjust scroll speed
+      slider.scrollLeft = scrollLeft - walk;
+    };
+
+    // Attach event listeners
+    slider.addEventListener("mousedown", mouseDownHandler);
+    slider.addEventListener("mouseleave", mouseLeaveHandler);
+    slider.addEventListener("mouseup", mouseUpHandler);
+    slider.addEventListener("mousemove", mouseMoveHandler);
+
+    // Cleanup event listeners on unmount
+    return () => {
+      slider.removeEventListener("mousedown", mouseDownHandler);
+      slider.removeEventListener("mouseleave", mouseLeaveHandler);
+      slider.removeEventListener("mouseup", mouseUpHandler);
+      slider.removeEventListener("mousemove", mouseMoveHandler);
+    };
+  }, [isDragged]);
 
   const parentVariants = {
     initial: {},
@@ -45,35 +214,249 @@ const Projects = () => {
     animate: { y: 0, opacity: 1, transition: { duration: 1 } },
   };
 
-  if (loading) {
-    return (
-      <main>
-        <h1>Loading...</h1>
-      </main>
-    );
-  }
-
-  if (error) {
-    return <main>{error}</main>;
-  }
-
   return (
-    <motion.div initial="initial" animate="animate" variants={parentVariants}>
-      <main>
-        <div id="project-cards">
-          {projects.map((project, index) => (
-            <motion.div
-              initial="initial"
-              animate="animate"
-              variants={childVariants}
-              key={index}
-            >
-              <ProjectPhotos project={project} index={index} />
-            </motion.div>
-          ))}
+    <motion.main initial="initial" animate="animate" variants={parentVariants}>
+      <motion.section
+        id="portfolio"
+        className="portfolio"
+        initial="initial"
+        animate="animate"
+        variants={childVariants}
+      >
+        <div className="wrap">
+          <header className="section-heading">
+            <h4>
+              <b>My Work</b>
+            </h4>
+            <nav className="portfolio-nav" aria-labelledby="portfolio">
+              <p className="portfolio-intro">
+                The projects I create illustrate the ideas and skills I am
+                developing over the course of my career as a developer.
+              </p>
+              <ul id="web_hidden">
+                <b>Frameworks:</b>
+                <li>
+                  <Link href="#vue">Nuxt/Vue</Link>
+                </li>
+                <li>
+                  <Link href="#react">React/Next</Link>
+                </li>
+                <li>
+                  <Link href="#other">Other</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+
+          <section className="portfolio-container">
+            <ol>
+              {display.map((col, i) => (
+                <li id={col.id} className="portfolio-content" key={i}>
+                  <div>
+                    <span>{i + 1}</span>
+                    <h3>{col.title}</h3>
+                    <div className="portfolio-index">
+                      <p className="portfolio__index-hide">
+                        * Scroll horizontally / drag right to view projects
+                      </p>
+                    </div>
+                  </div>
+                  <ul className="wrapper" ref={wrapperRef}>
+                    {col.content.map((cont, j) => (
+                      <li className="project" key={j}>
+                        <Link
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          href={col.link ? col.link : ""}
+                        >
+                          <Image
+                            src={comingSoon}
+                            alt="image of Music Material"
+                          />
+                          <p>
+                            <b>{cont.name}</b>
+                          </p>
+                          <p>{cont.description}</p>
+                        </Link>
+                        <ul className="project-tools"></ul>
+                        <div
+                          className="links"
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          {cont.link ? (
+                            <Link
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              href={cont.link}
+                              className="flex justify-center flex-col items-center hover:text-white"
+                            >
+                              <FaGlobeAfrica /> Demo
+                            </Link>
+                          ) : (
+                            ""
+                          )}
+                          {cont.github ? (
+                            <Link
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              href={cont.github}
+                              className="flex justify-center flex-col items-center hover:text-white"
+                            >
+                              {" "}
+                              <FaGithub /> Github
+                            </Link>
+                          ) : (
+                            ""
+                          )}
+                          {cont.figma ? (
+                            <Link
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              href={cont.figma}
+                              className="flex justify-center flex-col items-center hover:text-white"
+                            >
+                              {" "}
+                              <IoLogoFigma /> Figma
+                            </Link>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+
+              <li id="vue" className="portfolio-content">
+                <div className="portfolio-content-index">
+                  <div>
+                    <span>01</span>
+                  </div>
+                  <div className="portfolio-index">
+                    <h3>Nuxt/Vue</h3>
+                    <p className="portfolio__index-hide">
+                      * Scroll horizontally / drag right to view projects
+                    </p>
+                  </div>
+                </div>
+                <ul className="wrapper" ref={wrapperRef}>
+                  <li className="project">
+                    <Link
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://musicmaterial.netlify.app/"
+                    >
+                      <Image src={comingSoon} alt="image of Music Material" />
+                      <h4></h4>
+                      <p>
+                        A chess game allow users to play and fetch games using
+                      </p>
+                    </Link>
+                    <ul className="project-tools">
+                      <li>Nuxt</li>
+                      <li>Chess.com Api</li>
+                    </ul>
+                    <div className="links">
+                      <Link
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://musicmaterial.netlify.app/"
+                      >
+                        <i className="fas fa-external-link-alt"></i>Demo
+                      </Link>
+                    </div>
+                  </li>
+                  <li className="project">
+                    <Link
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://musicmaterial.netlify.app/"
+                    >
+                      <Image src={comingSoon} alt="image of Music Material" />
+                      <h4></h4>
+                      <p>
+                        A chess game allow users to play and fetch games using
+                        Chess.com API
+                      </p>
+                    </Link>
+                    <ul className="project-tools">
+                      <li>Nuxt</li>
+                      <li>Chess.com Api</li>
+                    </ul>
+                    <div className="links">
+                      <Link
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://musicmaterial.netlify.app/"
+                      >
+                        <i className="fas fa-external-link-alt"></i>Demo
+                      </Link>
+                    </div>
+                  </li>{" "}
+                  <li className="project">
+                    <Link
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://musicmaterial.netlify.app/"
+                    >
+                      <Image src={comingSoon} alt="image of Music Material" />
+                      <h4></h4>
+                      <p>
+                        A chess game allow users to play and fetch games using
+                        Chess.com API
+                      </p>
+                    </Link>
+                    <ul className="project-tools">
+                      <li>Nuxt</li>
+                      <li>Api</li>
+                    </ul>
+                    <div className="links">
+                      <Link
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://musicmaterial.netlify.app/"
+                      >
+                        <i className="fas fa-external-link-alt"></i>Demo
+                      </Link>
+                    </div>
+                  </li>{" "}
+                  <li className="project">
+                    <Link
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://musicmaterial.netlify.app/"
+                    >
+                      <Image src={comingSoon} alt="image of Music Material" />
+                      <h4></h4>
+                      <p>
+                        A chess game allow users to play and fetch games using
+                      </p>
+                    </Link>
+                    <ul className="project-tools">
+                      <li>Nuxt</li>
+                      <li>Chess.com Api</li>
+                    </ul>
+                    <div className="links">
+                      <Link
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://musicmaterial.netlify.app/"
+                      >
+                        <FaGlobeAfrica /> Demo
+                      </Link>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+            </ol>
+          </section>
         </div>
-      </main>
-    </motion.div>
+      </motion.section>
+    </motion.main>
   );
 };
 
