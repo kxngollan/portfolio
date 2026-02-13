be7672b72fc2188b2f21ea692a42cd57a2978144
Ollan Muza <128623143+kxngollan@users.noreply.github.com>
2026-02-13 12:28:55 +0000

    Create CV Page

---
+++ app/cv/page.tsx
@@ -0,0 +1,9 @@
+const page =()=>(
+    <main className="flex justify-center items-center w-screen h-screen">
+<iframe src="/cv/resume.pdf" 
+style={{width:"600px" ,height:"500px"}}></iframe>
+</main>
+
+)
+
+export default page
